664a90f9588097b549985c50c989dbefd02687d0
LiamRosvel <liamrosvel@gmail.com>
2017-01-14 15:37:26 -0700

    Se agrego script de Vanilla Js para mostrar el menu.

--- es6/scripts.js
+++ es6/scripts.js
@@ -1,10 +1,8 @@
-'use strict';
-
 //Scripts for active menu
 
 var toggle = document.getElementById('toggle-menu'),
     menu = document.getElementById('menu');
 
-toggle.addEventListener('click', function () {
+toggle.addEventListener('click', function(){
     menu.classList.toggle('active');
 });
